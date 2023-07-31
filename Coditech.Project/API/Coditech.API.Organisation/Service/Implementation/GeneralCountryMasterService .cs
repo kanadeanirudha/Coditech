125f8ea8e39f406f3370898cc1925bde0338606c
Tanuja.Kubitkar <tanujakubitkar02@gmail.com>
2023-07-30 23:28:10 -0700

    Code Checking For General Country Master Service

---
+++ Coditech.Project/API/Coditech.API.Organisation/Service/Implementation/GeneralCountryMasterService .cs
@@ -0,0 +1,148 @@
+﻿
+using Coditech.API.Data;
+using Coditech.Common.API.Model;
+using Coditech.Common.Exceptions;
+using Coditech.Common.Helper;
+using Coditech.Common.Helper.Utilities;
+using Coditech.Common.Logger;
+using Coditech.Resources;
+
+using System.Collections.Specialized;
+using System.Data;
+using System;
+
+using static Coditech.Common.Helper.HelperUtility;
+namespace Coditech.API.Service
+{
+    public class GeneralCountryMasterService : IGeneralCountryMasterService
+    {
+        protected readonly IServiceProvider _serviceProvider;
+        protected readonly ICoditechLogging _coditechLogging;
+        private readonly ICoditechRepository<GeneralCountryMaster> _generalCountryMasterRepository;
+        private readonly ICoditechRepository<OrganisationCentrewiseDepartment> _organisationCentrewiseDepartmentRepository;
+        public GeneralCountryMasterService(ICoditechLogging coditechLogging, IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+            _coditechLogging = coditechLogging;
+            _generalCountryMasterRepository = new CoditechRepository<GeneralCountryMaster>(_serviceProvider.GetService<Coditech_Entities>());
+            _organisationCentrewiseDepartmentRepository = new CoditechRepository<OrganisationCentrewiseDepartment>(_serviceProvider.GetService<Coditech_Entities>());
+        }
+        
+        public virtual GeneralCountryListModel GetCountryList(FilterCollection filters, NameValueCollection sorts, NameValueCollection expands, int pagingStart, int pagingLength)
+        {
+            //Bind the Filter, sorts & Paging details.
+            PageListModel pageListModel = new PageListModel(filters, sorts, pagingStart, pagingLength);
+            CoditechViewRepository<GeneralCountryModel> objStoredProc = new CoditechViewRepository<GeneralCountryModel>();
+            objStoredProc.SetParameter("@WhereClause", null/*pageListModel?.SPWhereClause*/, ParameterDirection.Input, DbType.String);
+            objStoredProc.SetParameter("@PageNo", pageListModel.PagingStart, ParameterDirection.Input, DbType.Int32);
+            objStoredProc.SetParameter("@Rows", pageListModel.PagingLength, ParameterDirection.Input, DbType.Int32);
+            objStoredProc.SetParameter("@Order_BY", pageListModel.OrderBy, ParameterDirection.Input, DbType.String);
+            objStoredProc.SetParameter("@RowsCount", pageListModel.TotalRowCount, ParameterDirection.Output, DbType.Int32);
+            List<GeneralCountryModel> CountryList = objStoredProc.ExecuteStoredProcedureList("RARIndia_GetCountryList @WhereClause,@Rows,@PageNo,@Order_BY,@RowsCount OUT", 4, out pageListModel.TotalRowCount)?.ToList();
+            GeneralCountryListModel listModel = new GeneralCountryListModel();
+
+            listModel.GeneralCountryList = CountryList?.Count > 0 ? CountryList : new List<GeneralCountryModel>();
+            listModel.BindPageListModel(pageListModel);
+            return listModel;
+        }
+        //Create Country.
+        public virtual GeneralCountryModel CreateCountry(GeneralCountryModel generalCountryModel)
+        {
+            if (IsNull(generalCountryModel))
+                throw new CoditechException(ErrorCodes.NullModel, GeneralResources.ModelNotNull);
+
+            if (IsCodeAlreadyExist(generalCountryModel.CountryCode))
+            {
+                throw new CoditechException(ErrorCodes.AlreadyExist, string.Format(GeneralResources.ErrorCodeExists, "Country Code"));
+            }
+
+            GeneralCountryMaster generalCountryMaster = generalCountryModel.FromModelToEntity<GeneralCountryMaster>();
+
+            //Create new Country and return it.
+            GeneralCountryMaster countryData = _generalCountryMasterRepository.Insert(generalCountryMaster);
+            if (countryData?.GeneralCountryMasterId > 0)
+            {
+                generalCountryModel.GeneralCountryMasterId = countryData.GeneralCountryMasterId;
+            }
+            else
+            {
+                generalCountryModel.HasError = true;
+                generalCountryModel.ErrorMessage = GeneralResources.ErrorFailedToCreate;
+            }
+            return generalCountryModel;
+        }
+
+        //Get Country by Country id.
+        public virtual GeneralCountryModel GetCountry(short countryId)
+        {
+            if (countryId <= 0)
+                throw new CoditechException(ErrorCodes.IdLessThanOne, string.Format(GeneralResources.ErrorIdLessThanOne, "CountryID"));
+
+            //Get the Country Details based on id.
+            GeneralCountryMaster generalCountryMaster = _generalCountryMasterRepository.Table.FirstOrDefault(x => x.GeneralCountryMasterId == countryId);
+            GeneralCountryModel generalCountryModel = generalCountryMaster?.FromEntityToModel<GeneralCountryModel>();
+            return generalCountryModel;
+        }
+
+        //Update Country.
+        public virtual bool UpdateCountry(GeneralCountryModel generalCountryModel)
+        {
+            if (IsNull(generalCountryModel))
+                throw new CoditechException(ErrorCodes.InvalidData, GeneralResources.ModelNotNull);
+
+            if (generalCountryModel.GeneralCountryMasterId < 1)
+                throw new CoditechException(ErrorCodes.IdLessThanOne, string.Format(GeneralResources.ErrorIdLessThanOne, "CountryID"));
+
+            GeneralCountryMaster generalCountryMaster = generalCountryModel.FromModelToEntity<GeneralCountryMaster>();
+
+            //Update Country
+            bool isCountryUpdated = _generalCountryMasterRepository.Update(generalCountryMaster);
+            if (!isCountryUpdated)
+            {
+                generalCountryModel.HasError = true;
+                generalCountryModel.ErrorMessage = GeneralResources.UpdateErrorMessage;
+            }
+            return isCountryUpdated;
+        }
+
+        //Delete Country.
+        public virtual bool DeleteCountry(ParameterModel parameterModel)
+        {
+            if (IsNull(parameterModel) || string.IsNullOrEmpty(parameterModel.Ids))
+                throw new CoditechException(ErrorCodes.IdLessThanOne, string.Format(GeneralResources.ErrorIdLessThanOne, "CountryID"));
+
+            CoditechViewRepository<View_ReturnBoolean> objStoredProc = new CoditechViewRepository<View_ReturnBoolean>();
+            objStoredProc.SetParameter("CountryId", parameterModel.Ids, ParameterDirection.Input, DbType.String);
+            objStoredProc.SetParameter("Status", null, ParameterDirection.Output, DbType.Int32);
+            int status = 0;
+            objStoredProc.ExecuteStoredProcedureList("RARIndia_DeleteCountry @CountryId,  @Status OUT", 1, out status);
+
+            return status == 1 ? true : false;
+        }
+
+        //Get Country list.
+        public virtual GeneralCountryListModel GetCountryByCentreCode(string centreCode)
+        {
+            GeneralCountryListModel list = new GeneralCountryListModel();
+            list.GeneralCountryList = (from a in _generalCountryMasterRepository.Table
+                                          join b in _organisationCentrewiseDepartmentRepository.Table
+                                          on a.GeneralCountryMasterId equals b.CountryId
+                                       where (b.CentreCode == centreCode || centreCode == null)
+                                          select new GeneralCountryModel()
+                                          {
+                                              GeneralCountryMasterId = a.GeneralCountryMasterId,
+                                              CountryName = a.CountryName,
+                                              CountryCode = a.CountryCode,
+                                              PrintShortDesc = a.PrintShortDesc
+                                          })?.ToList();
+            return list;
+        }
+
+        #region Protected Method
+
+        //Check if Country code is already present or not.
+        protected virtual bool IsCodeAlreadyExist(string countryName)
+         => _generalCountryMasterRepository.Table.Any(x => x.CountryName == countryName);
+        #endregion
+    }
+}
