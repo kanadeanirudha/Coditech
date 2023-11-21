9014d4b658a473634965664fb1974daa35ce2174
anirudha-kanade-amla <anirudha.kanade@amla.io>
2023-11-21 22:43:01 +0530

    Code correction

--- Coditech.Project/API/Comman/Coditech.API.Data/DataModel/User/UserMaster.cs
+++ Coditech.Project/API/Comman/Coditech.API.Data/DataModel/User/UserMaster.cs
@@ -10,8 +10,6 @@
         public string FirstName { get; set; }
         public string MiddleName { get; set; }
         public string LastName { get; set; }
-        public bool Gender { get; set; }
-        public DateTime DateOfBirth { get; set; }
         public bool IsActive { get; set; }
         public string DeviceToken { get; set; }
         public string LastModuleCode { get; set; }
