207091cb6a9d2b7cf6154e82b27210adea24b8d2
anirudha-kanade-amla <anirudha.kanade@amla.io>
2024-01-07 12:19:05 +0530

    Code correction

--- Coditech.Project/Admin/Coditech.Admin/ViewModel/GeneralMaster/GeneralOccupationMaster/GeneralOccupationViewModel.cs
+++ Coditech.Project/Admin/Coditech.Admin/ViewModel/GeneralMaster/GeneralOccupationMaster/GeneralOccupationViewModel.cs
@@ -6,9 +6,12 @@ namespace Coditech.Admin.ViewModel
     public class GeneralOccupationViewModel : BaseViewModel
     {
         public short GeneralOccupationMasterId { get; set; }
+
         [Required]
+        [MaxLength(60)]
         [Display(Name = "Occupation Name")]
         public string OccupationName { get; set; }
+
         [Display(Name = "Display Order")]
         [Required]
         public short DisplayOrder { get; set; }
