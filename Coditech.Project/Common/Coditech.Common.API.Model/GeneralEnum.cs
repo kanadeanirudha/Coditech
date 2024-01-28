5150b40fbe0bad83eca65f2d321ccbc1584b6a53
anirudha-kanade-amla <anirudha.kanade@amla.io>
2024-01-28 17:12:43 +0530

    Code correction

---
+++ Coditech.Project/Common/Coditech.Common.API.Model/GeneralEnum.cs
@@ -0,0 +1,67 @@
+﻿namespace Coditech.Common.Enum
+{
+    public enum DropdownTypeEnum
+    {
+        City,
+        AccessibleCentre,
+        CentrewiseDepartment,
+        Department,
+        Designation,
+        Organisation,
+        RegionalOffice,
+        Centre,
+        TaxGroup,
+        Country,
+        Region,
+        ModuleList,
+        MenuList,
+        Nationality,
+        Gender,
+        IndentificationType,
+        MaritalStatus,
+        BloodGroups,
+        Title,
+        Occupation,
+        GymGroup,
+        GymSource,
+        GymFollowupTypes
+    }
+
+    public enum GeneralSystemGlobleSettingEnum
+    {
+        IsGymMemberLogin,
+        IsEmployeeLogin,
+        ActiveProjectName,
+        DefaultPassword
+    }
+
+    public enum UserTypeEnum
+    {
+        Admin,
+        Employee,
+        Customer,
+        GymMember
+
+    }
+
+    public enum ActiveProjectNameEnum
+    {
+        GMS,
+        HMS
+    }
+
+    public enum MediaTypeEnum
+    {
+        Image,
+        File,
+        Video,
+        Audio
+    }
+
+    public enum AddressTypeEnum
+    {
+        PermanentAddress,
+        CorrespondanceAddress,
+        BusinessAddress        
+    }
+}
