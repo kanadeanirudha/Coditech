ab14d7b837a6265d9741d479fa72408b02839340
Aditi Utane <utaneaditi@gmail.com>
2024-02-04 18:14:47 +0530

    code checkin for gym memebership plan

--- Coditech.Project/Admin/Coditech.Admin/ViewModel/Gym/GymMembershipPlan/GymMembershipPlanViewModel.cs
+++ Coditech.Project/Admin/Coditech.Admin/ViewModel/Gym/GymMembershipPlan/GymMembershipPlanViewModel.cs
@@ -10,19 +10,30 @@ namespace Coditech.Admin.ViewModel
         {
         }
         public int GymMembershipPlanId { get; set; }
-        [MaxLength(100)]
+        [Required]
         public string MembershipPlanName { get; set; }
+        [MaxLength(100)]
+        [Required]
+        [Display(Name = "MembershipPlanName")]
         public decimal MaxCost { get; set; }
+        [Required]
         public decimal MinCost { get; set; }
+        [Required]
         public int PlanTypeEnumId { get; set; }
+        [Required]
         public string PlanType { get; set; }
-        public byte? PlanDurationInMonth { get; set; }
-        public short? PlanDurationInDays { get; set; }
-        public short? PlanDurationInSession { get; set; }
-        public bool IsRenewable { get; set; }
-        public bool IsTimebaseBiometricAccess { get; set; }
-        public TimeSpan? FromTime { get; set; }
-        public TimeSpan? ToTime { get; set; }
-        public bool IsActive { get; set; }
+        public byte PlanDurationInMonth { get; set; }
+        [Required]
+        public byte PlanDurationInDays { get; set; }
+        [Required]
+        public Int16 PlanDurationInSession { get; set; }
+        [Required]
+        public Boolean IsRenewable { get; set; }
+        [Required]
+        public Boolean IsTimebaseBiometricAccess { get; set; }
+        [Required]
+        public DateTime FromTime { get; set; }
+        public DateTime ToTime { get; set; }
+        public Boolean IsActive { get; set; }
     }
 }
