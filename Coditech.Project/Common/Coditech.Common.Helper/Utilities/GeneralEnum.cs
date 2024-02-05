1825649e90873ef36b1824c8c3b66eb372a2f339
anirudha-kanade-amla <anirudha.kanade@amla.io>
2024-02-05 08:12:02 +0530

    code correction

--- Coditech.Project/Common/Coditech.Common.Helper/Utilities/GeneralEnum.cs
+++ Coditech.Project/Common/Coditech.Common.Helper/Utilities/GeneralEnum.cs
@@ -24,7 +24,8 @@
         Occupation,
         GymGroup,
         GymSource,
-        GymFollowupTypes
+        GymFollowupTypes,
+        LeadStatus
     }
 
     public enum GeneralSystemGlobleSettingEnum
