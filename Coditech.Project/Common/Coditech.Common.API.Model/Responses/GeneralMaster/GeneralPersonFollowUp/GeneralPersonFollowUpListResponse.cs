9fca1fa4697260ea03e641a08bfde6597f8f2940
TanujaK <tanujaKubitkar02@gmail.com>
2024-02-12 21:37:59 +0530

    code

---
+++ Coditech.Project/Common/Coditech.Common.API.Model/Responses/GeneralMaster/GeneralPersonFollowUp/GeneralPersonFollowUpListResponse.cs
@@ -0,0 +1,9 @@
+﻿using Coditech.Common.API.Model.GeneralPerson.GeneralPersonFollowUp;
+
+namespace Coditech.Common.API.Model.Response
+{
+    public class GeneralPersonFollowUpListResponse : BaseListResponse
+    {
+        public List<GeneralPersonFollowUpModel> GeneralPersonFollowUpList { get; set; }
+    }
+}
---
+++ Coditech.Project/Common/Coditech.Common.API.Model/Responses/GeneralMaster/GeneralPersonFollowUp/GeneralPersonFollowUpResponse.cs
@@ -0,0 +1,10 @@
+﻿using Coditech.Common.API.Model.GeneralPerson.GeneralPersonFollowUp;
+
+namespace Coditech.Common.API.Model.Responses
+{
+    public class GeneralPersonFollowUpResponse : BaseResponse
+    {
+        public GeneralPersonFollowUpModel GeneralPersonFollowUpModel { get; set; }
+    }
+}
+
