7b4251c6b2f22ca335a56d161bc5edc84e9924db
anirudha kanade <anirudha.kanade@amla.io>
2024-03-27 09:40:25 +0530

    Addming new controller

---
+++ Coditech.Project/Admin/Coditech.Admin/Controllers/ErrorPageController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Coditech.Admin.Controllers
+{
+    public class ErrorPageController : BaseController
+    {
+
+        public virtual ActionResult Index()
+        {
+            return Redirect("/Error");
+        }
+
+        public virtual ActionResult PageNotFound()
+        {
+            return Redirect("/404");
+        }
+
+        public virtual ActionResult UnAuthorizedErrorRequest() => View("UnAuthorizedRequest");
+    }
+}
