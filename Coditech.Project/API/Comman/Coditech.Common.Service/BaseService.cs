6878bab1ba98c21bb1556a62812f0feb3227c73b
anirudha kanade <anirudha.kanade@amla.io>
2024-06-25 15:53:38 +0530

    Code correction

--- Coditech.Project/API/Comman/Coditech.Common.Service/BaseService.cs
+++ Coditech.Project/API/Comman/Coditech.Common.Service/BaseService.cs
@@ -163,9 +163,9 @@ namespace Coditech.Common.Service
                 registrationCode = registrationCode.Replace("<separator>", generalRunningNumbers.Separator);
                 registrationCode = registrationCode.Replace("<prefix>", generalRunningNumbers.Prefix);
                 registrationCode = registrationCode.Replace("<yyyy>", dateTime.Year.ToString());
-                registrationCode = registrationCode.Replace("<yy>", dateTime.Year.ToString());
+                registrationCode = registrationCode.Replace("<yy>", dateTime.Year.ToString().Substring(2,4));
                 registrationCode = registrationCode.Replace("<mm>", dateTime.Month.ToString());
-                registrationCode = registrationCode.Replace("<dd>", dateTime.Date.ToString());
+                registrationCode = registrationCode.Replace("<dd>", dateTime.Day.ToString());
                 registrationCode = registrationCode.Replace("<hh>", dateTime.Hour.ToString());
                 registrationCode = registrationCode.Replace("<min>", dateTime.Minute.ToString());
                 registrationCode = registrationCode.Replace("<sec>", dateTime.Second.ToString());
