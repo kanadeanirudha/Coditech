af2006033bfc6d7c80b6f80c87aa3f1dbdbaee1b
anirudha kanade <anirudha.kanade@amla.io>
2024-07-26 12:39:40 +0530

    code correction

--- Coditech.Project/Common/Coditech.Common.API.Model/GeneralMaster/CountryMaster/GeneralCountryModel.cs
+++ Coditech.Project/Common/Coditech.Common.API.Model/GeneralMaster/CountryMaster/GeneralCountryModel.cs
@@ -2,13 +2,10 @@
 {
     public class GeneralCountryModel : BaseModel
     {
-        public GeneralCountryModel()
-        {
-
-        }
         public short GeneralCountryMasterId { get; set; }
         public string CountryName { get; set; }
         public string CountryCode { get; set; }
+        public string CallingCode { get; set; }
         public bool DefaultFlag { get; set; }
         public bool IsUserDefined { get; set; } = false;
         public short SeqNo { get; set; }
