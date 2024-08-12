f3e22d714a1773e1c25d15f9e89ab0a45af65523
Anirudha Kanade <anirudhakanade1007@gmail.com>
2024-08-12 18:14:32 +0530

    code check-in for Coditech.Engine.Gazette

---
+++ Coditech.Project/API/Coditech.Engine.Gazette/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Coditech.Common.Helper.Utilities;
+
+namespace Coditech.API.Mapper
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<FilterTuple, FilterDataTuple>().ReverseMap();
+        }
+    }
+}
