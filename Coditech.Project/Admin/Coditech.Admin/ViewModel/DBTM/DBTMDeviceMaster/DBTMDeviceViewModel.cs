0e054dfbd451fab516d0b4fa250ba35d5a045b43
TanujaK <tanujaKubitkar02@gmail.com>
2024-09-26 12:09:58 +0530

    Code Checkin DBTMDeviceMaster.

---
+++ Coditech.Project/Admin/Coditech.Admin/ViewModel/DBTM/DBTMDeviceMaster/DBTMDeviceViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Coditech.Common.Helper;
+
+namespace Coditech.Admin.ViewModel
+{
+    public class DBTMDeviceViewModel : BaseViewModel
+    {
+        public long DBTMDeviceMasterId { get; set; }
+        public string DeviceName { get; set; }
+        public string DeviceSerialCode { get; set; }
+        public string Description { get; set; }
+        public string ManufacturedBy { get; set; }
+        public int Status { get; set; }
+        public bool IsMasterDevice { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public short WarrantyExpirationPeriodInMonth { get; set; }
+        public string AdditionalFeatures { get; set; }
+    }
+}
