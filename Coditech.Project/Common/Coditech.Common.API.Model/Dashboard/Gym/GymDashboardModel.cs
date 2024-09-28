2e98e12972a2b666c92f748f89a2486b790d70a3
Anirudha Kanade <anirudhakanade1007@gmail.com>
2024-09-28 17:06:51 +0530

    Code correction

--- Coditech.Project/Common/Coditech.Common.API.Model/Dashboard/Gym/GymDashboardModel.cs
+++ Coditech.Project/Common/Coditech.Common.API.Model/Dashboard/Gym/GymDashboardModel.cs
@@ -7,8 +7,11 @@
         public int TotalMemberCount { get; set; }
         public Decimal ToadysTotalCollection { get; set; }
         public Decimal DaywiseTotalCollection { get; set; }
+        public Decimal TotalLeads { get; set; }
         public Decimal DigitalPaymentCollection { get; set; }
         public Decimal ManualPaymentCollection { get; set; }
         public List<GymTransactionOverviewModel> TransactionOverviewList { get; set; }
+        public List<GymTransactionOverviewModel> RevenueByPaymentModeList { get; set; }
+        public List<GymTransactionOverviewModel> YearlyFinancialOverviewList { get; set; }
     }
 }
--- Coditech.Project/Common/Coditech.Common.API.Model/Dashboard/Gym/GymTransactionOverviewModel.cs
+++ Coditech.Project/Common/Coditech.Common.API.Model/Dashboard/Gym/GymTransactionOverviewModel.cs
@@ -3,6 +3,8 @@
     public class GymTransactionOverviewModel : BaseModel
     {
         public DateTime TransactionDate { get; set; }
+        public string PaymentType { get; set; }
+        public string MonthName { get; set; }
         public Decimal NetAmount { get; set; }
         public Decimal TaxAmount { get; set; }
         public Decimal DiscountAmount { get; set; }
