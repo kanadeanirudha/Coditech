582a424603e7b74234a88069e0a84b4651008d5e
Gajendra Pardhi <gajjupardhi58@gmail.com>
2024-10-14 16:28:48 +0530

    GMSD-88: EMP - Missing Sorting Functionality for Gender Column in Employee List

--- Coditech.Project/Admin/Coditech.Admin/Agents/Implementation/Employee/EmployeeMasterAgent.cs
+++ Coditech.Project/Admin/Coditech.Admin/Agents/Implementation/Employee/EmployeeMasterAgent.cs
@@ -229,7 +229,8 @@ namespace Coditech.Admin.Agents
             datatableColumnList.Add(new DatatableColumns()
             {
                 ColumnName = "Gender",
-                ColumnCode = "Gender",
+                ColumnCode = "GenderEnumId",
+                IsSortable = true,
             });
             datatableColumnList.Add(new DatatableColumns()
             {
