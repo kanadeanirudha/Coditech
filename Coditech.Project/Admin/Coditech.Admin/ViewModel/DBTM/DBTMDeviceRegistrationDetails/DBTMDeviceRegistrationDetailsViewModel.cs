e5de76a964314990f5f80a5fa45b6349ac38eeef
Gajendra Pardhi <gajjupardhi58@gmail.com>
2024-12-11 19:06:35 +0530

    Code Check In For -DBTMDeviceRegistrationDetails

--- Coditech.Project/Admin/Coditech.Admin/ViewModel/DBTM/DBTMDeviceRegistrationDetails/DBTMDeviceRegistrationDetailsViewModel.cs
+++ Coditech.Project/Admin/Coditech.Admin/ViewModel/DBTM/DBTMDeviceRegistrationDetails/DBTMDeviceRegistrationDetailsViewModel.cs
@@ -8,8 +8,15 @@ namespace Coditech.Admin.ViewModel
         public long DBTMDeviceRegistrationDetailId { get; set; }
         [Display(Name = "Device")]
         public long DBTMDeviceMasterId { get; set; }
+        [Required]
+        public string DeviceSerialCode { get; set; }
+        [Required]
+        [MaxLength(200)]
+        public string DeviceName { get; set; }
         public string UserType { get; set; }
         public long EntityId { get; set; }
         public DateTime PurchaseDate { get; set; }
+        public bool IsMasterDevice { get; set; }
+            
     }
 }
