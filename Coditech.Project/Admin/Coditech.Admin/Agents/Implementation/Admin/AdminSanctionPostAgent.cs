00e02c88d23380ffdd52690250cfb1760f5dfe83
Gajendra Pardhi <gajjupardhi58@gmail.com>
2024-12-13 15:23:44 +0530

    GMSD-292: ADM -Inconsistent Heading for "IsActive" Column in Admin Module.

--- Coditech.Project/Admin/Coditech.Admin/Agents/Implementation/Admin/AdminSanctionPostAgent.cs
+++ Coditech.Project/Admin/Coditech.Admin/Agents/Implementation/Admin/AdminSanctionPostAgent.cs
@@ -166,7 +166,7 @@ namespace Coditech.Admin.Agents
             });
             datatableColumnList.Add(new DatatableColumns()
             {
-                ColumnName = "Is Active",
+                ColumnName = "IsActive",
                 ColumnCode = "IsActive",
                 IsSortable = true,
             });
