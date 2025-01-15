4a5d02b4f32bd771dc0bb6db3314bbd50b696bcb
anuragjambhulkar <jambhulkaranurag12@gmail.com>
2025-01-15 16:31:18 +0530

    Check In For AccGLSetupNarration

--- Coditech.Project/API/Comman/Coditech.API.Data/DataModel/Accounts/AccGLSetupNarration.cs
+++ Coditech.Project/API/Comman/Coditech.API.Data/DataModel/Accounts/AccGLSetupNarration.cs
@@ -9,7 +9,7 @@ namespace Coditech.API.Data
         public string NarrationDescription { get; set; }
         public string CentreCode { get; set; }
         public bool IsActive { get; set; }
-        public bool IsSystemGenerated{ get; set; }
+        public bool IsSystemGenerated { get; set; }
         public Nullable<long> CreatedBy { get; set; }
         public Nullable<System.DateTime> CreatedDate { get; set; }
         public Nullable<long> ModifiedBy { get; set; }
