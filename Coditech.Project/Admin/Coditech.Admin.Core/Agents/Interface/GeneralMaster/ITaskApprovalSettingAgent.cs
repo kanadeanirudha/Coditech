7fe4b91f0c23c5166120e034b2c3085a75f8ec38
Nikita Ghansawant <nikitacoditech8149@gmail.com>
2025-01-18 13:30:19 +0530

    Code check in for TaskApprovalSettingUpdate

--- Coditech.Project/Admin/Coditech.Admin.Core/Agents/Interface/GeneralMaster/ITaskApprovalSettingAgent.cs
+++ Coditech.Project/Admin/Coditech.Admin.Core/Agents/Interface/GeneralMaster/ITaskApprovalSettingAgent.cs
@@ -34,5 +34,28 @@ namespace Coditech.Admin.Agents
         /// <param name="taskApprovalSettingViewModel">TaskApproval Setting  View Model.</param>
         /// <returns>Returns created model.</returns>
         TaskApprovalSettingViewModel AddUpdateTaskApprovalSetting(TaskApprovalSettingViewModel taskApprovalSettingViewModel);
+
+        /// <summary>
+        /// Get TaskApprovalSetting by TaskApprovalSettingId.
+        /// </summary>
+        /// <param name="centreCode">centreCode</param>
+        /// <param name="taskMasterId">taskMasterId</param>
+        /// <param name="taskApprovalSettingId">taskApprovalSettingId</param>
+        TaskApprovalSettingViewModel GetUpdateTaskApprovalSetting(short taskMasterId, string centreCode, int taskApprovalSettingId);
+
+        /// <summary>
+        /// Update TaskApprovalSetting.
+        /// </summary>
+        /// <param name="taskApprovalSettingViewModel">ticketMasterViewModel.</param>
+        /// <returns>Returns updated TaskApprovalSettingViewModel</returns>
+        TaskApprovalSettingViewModel UpdateTaskApprovalSetting(TaskApprovalSettingViewModel taskApprovalSettingViewModel);
+
+        /// <summary>
+        /// Delete TaskApprovalSetting.
+        /// </summary>
+        /// <param name="taskApprovalSettingIds">taskApprovalSettingIds.</param>
+        /// <returns>Returns true if deleted successfully else return false.</returns>
+        bool DeleteTaskApprovalSetting(string taskApprovalSettingIds, out string errorMessage);
+        
     }
 }
