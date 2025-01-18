93e19349be2fbe4c62cf0f12acec31d469763880
Bhavesh Shambharkar <148750630+BhaveshS777@users.noreply.github.com>
2025-01-18 15:43:35 +0530

    Code Check In For GeneralFinancialYear

--- Coditech.Project/API/Coditech.Engine.Organisation/Service/Implementation/GeneralMaster/GeneralFinancialYearMasterService.cs
+++ Coditech.Project/API/Coditech.Engine.Organisation/Service/Implementation/GeneralMaster/GeneralFinancialYearMasterService.cs
@@ -27,10 +27,13 @@ namespace Coditech.API.Service
 
         public virtual GeneralFinancialYearListModel GetFinancialYearList(FilterCollection filters, NameValueCollection sorts, NameValueCollection expands, int pagingStart, int pagingLength)
         {
+
+            string selectedCentreCode = filters?.Find(x => string.Equals(x.FilterName, FilterKeys.SelectedCentreCode, StringComparison.CurrentCultureIgnoreCase))?.FilterValue;
+            filters.RemoveAll(x => x.FilterName == FilterKeys.SelectedCentreCode);
             //Bind the Filter, sorts & Paging details.
             PageListModel pageListModel = new PageListModel(filters, sorts, pagingStart, pagingLength);
             CoditechViewRepository<GeneralFinancialYearModel> objStoredProc = new CoditechViewRepository<GeneralFinancialYearModel>(_serviceProvider.GetService<Coditech_Entities>());
-            objStoredProc.SetParameter("@CentreCode", "", ParameterDirection.Input, DbType.String);
+            objStoredProc.SetParameter("@CentreCode", selectedCentreCode, ParameterDirection.Input, DbType.String);
             objStoredProc.SetParameter("@WhereClause", pageListModel?.SPWhereClause, ParameterDirection.Input, DbType.String);
             objStoredProc.SetParameter("@PageNo", pageListModel.PagingStart, ParameterDirection.Input, DbType.Int32);
             objStoredProc.SetParameter("@Rows", pageListModel.PagingLength, ParameterDirection.Input, DbType.Int32);
