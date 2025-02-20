5278edc7f35e21bcdbcc5a09c5a8ac4ab637111c
Gajendra Pardhi <gajjupardhi58@gmail.com>
2025-02-20 19:18:34 +0530

    Code Check In For - AccSetupGL-WithCategoryList

---
+++ Coditech.Project/API/Comman/Coditech.API.Data/DataModel/Accounts/AccSetupCategory.cs
@@ -0,0 +1,15 @@
+﻿namespace Coditech.API.Data
+{
+    public class AccSetupCategory
+    {
+        public short AccSetupCategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryCode { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsSystemGenerated { get; set; }
+        public Nullable<long> CreatedBy { get; set; }
+        public Nullable<System.DateTime> CreatedDate { get; set; }
+        public Nullable<long> ModifiedBy { get; set; }
+        public Nullable<System.DateTime> ModifiedDate { get; set; }
+    }
+}
--- Coditech.Project/Common/Coditech.Common.API.Model/Accounts/AccSetupGL/AccSetupGLModel.cs
+++ Coditech.Project/Common/Coditech.Common.API.Model/Accounts/AccSetupGL/AccSetupGLModel.cs
@@ -7,12 +7,17 @@
         public string GLName { get; set; }
         public int? ParentAccSetupGLId { get; set; }
         public string CategoryCode { get; set; }
+        public string GLCode { get; set; }
         public decimal OpeningBalance { get; set; }
         public bool IsGroup { get; set; }
         public int AccSetupBalancesheetId { get; set; }
         public byte AccSetupChartOfAccountTemplateId { get; set; }
         public byte AccSetupBalanceSheetTypeId { get; set; }
         public string SelectedCentreCode { get; set; }
+        public short AccSetupCategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool IsActive { get; set; }
+        public List<AccSetupCategoryModel> AccSetupCategoryList { get; set; }
         public List<AccSetupGLModel> AccSetupGLList { get; set; }
         public List<AccSetupGLModel> SubAccounts { get; set; } = new List<AccSetupGLModel>();
     }
