51ec3881bb1bdb231f5aae6b2e03c7809038634f
Nikita Ghansawant <nikitacoditech8149@gmail.com>
2025-02-26 11:48:21 +0530

    Code check in for joining Code

---
+++ Coditech.Project/Admin/Coditech.Admin/wwwroot/js/Coditech/Organisation/OrganisationCentrewiseJoiningCode.js
@@ -0,0 +1,33 @@
+﻿var OrganisationCentrewiseJoiningCode = {
+    Initialize: function () {
+        OrganisationCentrewiseJoiningCode.constructor();
+    },
+
+    constructor: function () {
+    },
+
+    GetOrganisationCentrewiseJoiningCodeSend: function (modelPopContentId) {
+        CoditechCommon.ShowLodder();
+        $.ajax({
+            cache: false,
+            type: "GET",
+            dataType: "html",
+            url: "/OrganisationCentrewiseJoiningCode/GetOrganisationCentrewiseJoiningCodeSend",            
+            contentType: "application/json; charset=utf-8",
+            success: function (result) {
+                $('#' + modelPopContentId).html(result);
+                CoditechCommon.HideLodder();
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                if (xhr.status == "401" || xhr.status == "403") {
+                    {
+                        location.reload();
+                    }
+                    CoditechNotification.DisplayNotificationMessage("Failed to load details.", "error");
+                    CoditechCommon.HideLodder();
+                }
+            }
+        });
+        $("#frmSendDetails").submit();
+    },
+}
