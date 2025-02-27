71f825cfc3794e07e59a2c16e3488d7115aae082
Anirudha Kanade <anirudhakanade1007@gmail.com>
2025-02-27 11:56:03 +0530

    code correction

--- Coditech.Project/API/Comman/Coditech.API.Client/Endpoint/Gym/GymDashboardEndpoint.cs
+++ Coditech.Project/API/Comman/Coditech.API.Client/Endpoint/Gym/GymDashboardEndpoint.cs
@@ -6,6 +6,6 @@ namespace Coditech.API.Endpoint
     public class GymDashboardEndpoint : BaseEndpoint
     {
         public string GetGymDashboardDetailsAsync(short numberOfDaysRecord, int selectedAdminRoleMasterId,long userMasterId) =>
-            $"{CoditechAdminSettings.CoditechGymManagementSystemApiRootUri}/GymDashboardController/GetGymDashboardDetails?selectedAdminRoleMasterId={selectedAdminRoleMasterId}&userMasterId={userMasterId}";
+            $"{CoditechAdminSettings.CoditechGymManagementSystemApiRootUri}/GymDashboardController/GetGymDashboardDetails?numberOfDaysRecord={numberOfDaysRecord}&selectedAdminRoleMasterId={selectedAdminRoleMasterId}&userMasterId={userMasterId}";
     }
 }
