12d1bd2d82b2832838dff627c1f8e58212abf73e
Anirudha Kanade <anirudhakanade1007@gmail.com>
2025-02-27 21:22:58 +0530

    code correction

--- Coditech.Project/Admin/Coditech.Admin.Core/Controllers/DashboardController.cs
+++ Coditech.Project/Admin/Coditech.Admin.Core/Controllers/DashboardController.cs
@@ -22,7 +22,7 @@ namespace Coditech.Admin.Controllers
         [HttpGet]
         public virtual IActionResult Index(short numberOfDaysRecord)
         {
-            DashboardViewModel dashboardViewModel = _dashboardAgent.GetDashboardDetails(numberOfDaysRecord);
+            DashboardViewModel dashboardViewModel = _dashboardAgent.GetDashboardDetails();
             if (IsNotNull(dashboardViewModel) && !string.IsNullOrEmpty(dashboardViewModel.DashboardFormEnumCode))
             {
                 if (dashboardViewModel.DashboardFormEnumCode.Equals(DashboardFormEnum.GymOwnerDashboard.ToString(), StringComparison.InvariantCultureIgnoreCase))
