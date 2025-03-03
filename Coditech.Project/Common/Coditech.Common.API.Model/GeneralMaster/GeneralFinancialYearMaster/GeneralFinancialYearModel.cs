3225b008439af033bca03b924a63e7dae9751f06
Bhavesh Shambharkar <148750630+BhaveshS777@users.noreply.github.com>
2025-03-03 18:29:25 +0530

    Check-in For GeneralFinancialYear.

--- Coditech.Project/Common/Coditech.Common.API.Model/GeneralMaster/GeneralFinancialYearMaster/GeneralFinancialYearModel.cs
+++ Coditech.Project/Common/Coditech.Common.API.Model/GeneralMaster/GeneralFinancialYearMaster/GeneralFinancialYearModel.cs
@@ -5,5 +5,8 @@
         public short GeneralFinancialYearId { get; set; }
         public DateTime FromDate { get; set; }
         public DateTime ToDate { get; set; }
+        public string CentreCode { get; set; }
+        public bool IsYearEnd { get; set; }
+        public bool IsCurrentFinancialYear { get; set; }
     }
 }
