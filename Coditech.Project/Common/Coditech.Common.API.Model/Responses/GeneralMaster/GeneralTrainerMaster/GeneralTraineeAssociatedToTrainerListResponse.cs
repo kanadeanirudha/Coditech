92df2c4b0b5111a57bbc3abb94ab16debefd257e
Tanuja Kubitkar <tanujakubitkar02@gmail.com>
2025-06-11 13:23:00 +0530

    Code Correction.

--- Coditech.Project/Common/Coditech.Common.API.Model/Responses/GeneralMaster/GeneralTrainerMaster/GeneralTraineeAssociatedToTrainerListResponse.cs
+++ Coditech.Project/Common/Coditech.Common.API.Model/Responses/GeneralMaster/GeneralTrainerMaster/GeneralTraineeAssociatedToTrainerListResponse.cs
@@ -6,5 +6,6 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public bool IsEntityActive { get; set; }
+        public string Custom1 { get; set; }
     }
 }
