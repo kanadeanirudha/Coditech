5c14a2b2507154d9aa9885fb4048f8f00dd06f5b
Tanuja Kubitkar <tanujakubitkar02@gmail.com>
2025-06-13 18:27:05 +0530

    Code Correction.

--- Coditech.Project/API/Comman/Coditech.API.Client/Endpoint/GeneralMaster/GeneralBatchEndpoint.cs
+++ Coditech.Project/API/Comman/Coditech.API.Client/Endpoint/GeneralMaster/GeneralBatchEndpoint.cs
@@ -8,7 +8,7 @@ namespace Coditech.API.Endpoint
     {
         public string ListAsync(string selectedCentreCode, long userId, IEnumerable<string> expand, IEnumerable<FilterTuple> filter, IDictionary<string, string> sort, int? pageIndex, int? pageSize)
         {
-            string endpoint = $"{CoditechAdminSettings.CoditechOrganisationApiRootUri}/GeneralBatchMaster/GetBatchList?selectedCentreCode={selectedCentreCode}userId={userId}{BuildEndpointQueryString(true,expand, filter, sort, pageIndex, pageSize)}";
+            string endpoint = $"{CoditechAdminSettings.CoditechOrganisationApiRootUri}/GeneralBatchMaster/GetBatchList?selectedCentreCode={selectedCentreCode}&userId={userId}{BuildEndpointQueryString(true,expand, filter, sort, pageIndex, pageSize)}";
             return endpoint;
         }
         public string CreateGeneralBatchAsync() =>
