92caafd6964981a9412e81765b7e855bbbdece1f
Nikita Ghansawant <nikitacoditech8149@gmail.com>
2025-07-25 16:52:46 +0530

    Code check in for Generalbatchmaster  entity model

--- Coditech.Project/API/Comman/Coditech.API.Data/DataModel/GeneralMaster/GeneralBatchMaster.cs
+++ Coditech.Project/API/Comman/Coditech.API.Data/DataModel/GeneralMaster/GeneralBatchMaster.cs
@@ -8,8 +8,12 @@ namespace Coditech.API.Data
         public int GeneralBatchMasterId { get; set; }
         public string CentreCode { get; set; }
         public string BatchName { get; set; }
-        public TimeSpan BatchTime { get; set; }
         public TimeSpan BatchStartTime { get; set; }
+        public TimeSpan BatchEndTime { get; set; }
+        public string BatchFrequency { get; set; }
+        public string WeekDays { get; set; }
+        public Nullable<DateTime> BatchStartDate { get; set; }
+        public Nullable<DateTime> BatchExpireDate { get; set; }
         public bool IsActive { get; set; }
         public Nullable<long> CreatedBy { get; set; }
         public Nullable<System.DateTime> CreatedDate { get; set; }
