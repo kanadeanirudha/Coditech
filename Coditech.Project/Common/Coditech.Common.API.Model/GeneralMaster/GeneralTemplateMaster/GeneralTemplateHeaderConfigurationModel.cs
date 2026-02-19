255f15a091de36ed77461173fdd6b9e0fb2ad617
Nikita Ghansawant <nikitacoditech8149@gmail.com>
2026-02-19 12:34:09 +0530

    Code check for  HeaderCode added in generaltemaplateheaderconfiguration

--- Coditech.Project/Common/Coditech.Common.API.Model/GeneralMaster/GeneralTemplateMaster/GeneralTemplateHeaderConfigurationModel.cs
+++ Coditech.Project/Common/Coditech.Common.API.Model/GeneralMaster/GeneralTemplateMaster/GeneralTemplateHeaderConfigurationModel.cs
@@ -7,6 +7,7 @@ namespace Coditech.Common.API.Model
         public int GeneralTemplateHeaderConfigurationId { get; set; }
         public string TemplateCode { get; set; }
         public string HeaderName { get; set; }
+        public string HeaderCode { get; set; }
         public string HeaderType { get; set; }
         public string CentreCode { get; set; }
         public int OrderBy { get; set; }
