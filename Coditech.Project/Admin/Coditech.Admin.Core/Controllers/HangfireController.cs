cb14229f32e9c4f40fd8bbcf457a3a764eb9ae7e
Nikita Ghansawant <nikitacoditech8149@gmail.com>
2026-04-13 17:24:15 +0530

    code check in for Hangfire Dashboard changes

---
+++ Coditech.Project/Admin/Coditech.Admin.Core/Controllers/HangfireController.cs
@@ -0,0 +1,17 @@
+﻿using Coditech.Admin.Agents;
+using Coditech.Admin.Utilities;
+using Coditech.Admin.ViewModel;
+using Coditech.Common.Exceptions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+namespace Coditech.Admin.Controllers
+{
+    public class HangfireController : BaseController
+    {
+        [HttpGet]
+        public IActionResult Dashboard()
+        {
+            return View("~/Views/Hangfire/HangfireDashboard.cshtml");
+        }
+    }
+}
